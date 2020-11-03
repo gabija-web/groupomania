e907abed874a629b27558429e57af13aeedc7519
gabija-web <gabijasmolskiene@gmail.com>
2020-11-03 23:23:39 +0000

    logged out button functionality

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated} = require('../config/auth');
 
 //main page
 router.get('/', (req, res) => res.render('welcome'));
 
 //dashboard
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+    res.render('dashboard', {
+        name: req.user.name
+    }));
 
 module.exports = router;
